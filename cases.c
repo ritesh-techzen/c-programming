3f590c022022b9c9f092122f1d3b33043e2c4c18
Ritesh kr shah <166778450+ritesh-techzen@users.noreply.github.com>
2024-04-17 23:14:49 -0700

    Initial commit

---
+++ cases.c
@@ -0,0 +1,37 @@
+/*i   ntegers , characters, float value
+#include <stdio.h>
+int main()
+{
+    int age= 19;
+    printf("your age is %d:",age);
+    
+}
+*/
+
+
+/* 2       to print character values
+
+
+#include<stdio.h>
+int main()
+{
+    char star='*';
+    printf ("star is %c\n",star);
+
+}
+*/
+
+// 3      float values
+
+
+
+#include<stdio.h>
+int main()
+{
+    float pi ='2.34';
+    printf ("float value  is %f\n",pi);
+
+
+}
+
+
